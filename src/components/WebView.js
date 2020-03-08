9f0f125e9be2613ca16103e8d17312e2f30eac06
Huberts <huberts@Hubertss-iMac.lan>
2020-03-08 14:27:17 +0200

    Catch links in native and open in browser (untested)

--- src/components/WebView.js
+++ src/components/WebView.js
@@ -1,14 +1,42 @@
-import React from "react"
+import React, { useRef, useCallback } from "react"
+import { Linking } from 'react-native'
 import { WebView as RDWebView } from 'react-native-webview'
 
 const WebView = ({
   forwardRef,  // I do this so I can be consistent with WebView.web.js
   ...props
-}) => (
-  <RDWebView
-    ref={forwardRef}
-    {...props}
-  />
-)
+}) => {
+
+  const webviewRef = useRef()
+
+  const onNavigationStateChange = useCallback(
+    ({ url }) => {
+      if(url) {
+        webviewRef.current.stopLoading()
+        Linking.openURL(url)
+      }
+    },
+    [],
+  )
+
+  const setRef = useCallback(
+    ref => {
+      webviewRef.current = ref
+      if(forwardRef) {
+        forwardRef.current = ref
+      }
+    },
+    [],
+  )
+
+  return (
+    <RDWebView
+      ref={setRef}
+      onNavigationStateChange={onNavigationStateChange}
+      {...props}
+    />
+  )
+
+}
 
 export default WebView
